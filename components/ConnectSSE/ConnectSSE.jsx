6409a0f8497c24c38e2e748ee42a76add8fd2586
Troy Pernía <troy00pernia@gmail.com>
2024-03-27 11:35:49 -0400

    change

--- components/ConnectSSE/ConnectSSE.jsx
+++ components/ConnectSSE/ConnectSSE.jsx
@@ -2,19 +2,60 @@
 
 import { useState, useEffect } from "react"
 
-function ConnectSSE() {
+function ConnectSSE({ setConnected }) {
 
     const [ events, setEvents] = useState([])
 
     useEffect(() => {
-        const eventSource = new EventSource('http://localhost:3001/sse')
+        console.log('events:', events)
+        if (events.length) setConnected(events[0])
+    }, [events])
 
+    useEffect(() => {
+        const eventSource = new EventSource('http://localhost:3002/sse')
+
+        eventSource.addEventListener('open', () => {
+          console.log('SSE opened!');
+        });
+
+        // usando eventSource.onmessage
         eventSource.onmessage = (event) => {
+            // const data = JSON.parse(event.data)
+            console.log('data:', event.data)
             setEvents(prevEvents => [...prevEvents, event.data])
         }
 
+        // usando enventos personalizados
+        eventSource.addEventListener = ( 'miEvento', (event) => {
+            const eventData = JSON.parse(event.data)
+            const mensaje = eventData.message
+            setEvents(prevEvents => [...prevEvents, mensaje])
+        })
+
+        // const source = new EventSource('http://localhost:3002/');
+    
+        // source.addEventListener('open', () => {
+        //   console.log('SSE opened!');
+        // });
+    
+        // source.onopen = () => {
+        //     console.log('SSE opened!')
+        // }
+
+        // source.addEventListener('message', (e) => {
+        //   const data = JSON.parse(e.data);
+        //   if(data[0]){
+        //     setPairList(data[0]);
+        //     setFetchList(true);
+        //     return
+        //   }
+        //   setData(data);
+        // });
+
         return () => {
+            console.log('SSE closed')
             eventSource.close()
+            // source.close()
         }
 
     }, [])
